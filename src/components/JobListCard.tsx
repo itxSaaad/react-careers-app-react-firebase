ff4f75c4064231aa5257f4da4eba234b8104e869
Muhammad Saad <saadstudent.cs@gmail.com>
2024-06-21 19:53:56 +0500

    Added: State to Description length of Job

--- src/components/JobListCard.tsx
+++ src/components/JobListCard.tsx
@@ -19,7 +19,18 @@ interface JobListProps {
   index: number;
 }
 
+import { useState } from 'react';
+import { FaMapMarkerAlt } from 'react-icons/fa';
+
 export default function JobListCard({ Job, index }: JobListProps) {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  let description = Job.description;
+
+  if (!showFullDescription && description.length > 90) {
+    description = description.slice(0, 90) + '...';
+  }
+
   return (
     <div key={index} className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
@@ -28,7 +39,13 @@ export default function JobListCard({ Job, index }: JobListProps) {
           <h3 className="text-xl font-bold">{Job.title}</h3>
         </div>
 
-        <div className="mb-5">{Job.description}</div>
+        <div className="mb-5">{description}</div>
+        <button
+          onClick={() => setShowFullDescription(!showFullDescription)}
+          className="text-indigo-500 mb-5 hover:underline hover:text-indigo-600"
+        >
+          {showFullDescription ? 'Less' : 'More'}
+        </button>
 
         <h3 className="text-indigo-500 mb-2">{Job.salary}</h3>
 
@@ -36,7 +53,7 @@ export default function JobListCard({ Job, index }: JobListProps) {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <i className="fa-solid fa-location-dot text-lg"></i>
+            <FaMapMarkerAlt className="inline text-lg mb-1 mr-1" />
             {Job.location}
           </div>
           <a
